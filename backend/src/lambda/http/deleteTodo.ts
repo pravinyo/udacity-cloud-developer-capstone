c0475e31f827758a930022e4f6348816f1f886ff
Pravinyo <pravinyo12@gmail.com>
2020-04-06 21:14:18 +0530

    delete todo implemented

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,32 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { deleteTodo } from '../../domain/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { TodoDeleteResponse } from '../../models/TodoDeleteResponse'
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  //Remove a TODO item by id
+  const response : TodoDeleteResponse = await deleteTodo(todoId,jwtToken)
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify({
+      response
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
