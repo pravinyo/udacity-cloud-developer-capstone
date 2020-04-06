b346a57657facb2b071ab245bf4dde36621cfb08
Pravinyo <pravinyo12@gmail.com>
2020-04-06 21:15:06 +0530

    update todo implemented

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,35 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../domain/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  // Update a TODO item with the provided id using values in the "updatedTodo" object
+  const response = await updateTodo(todoId,updatedTodo,jwtToken)
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify({
+      response
+    })
+  }
+})
+
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
