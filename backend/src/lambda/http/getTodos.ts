e61521368c14320347ebe1f4941b243ba9f972ae
Pravinyo <pravinyo12@gmail.com>
2020-04-06 21:14:51 +0530

    get todo implemented

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,30 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getAllTodos } from '../../domain/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // Get all TODO items for a current user
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const results = await getAllTodos(jwtToken)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      items: results
+    })
+  }
+
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
