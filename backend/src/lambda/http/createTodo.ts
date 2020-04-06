2f188a39d8037030caa9f618b147c1701e066043
Pravinyo <pravinyo12@gmail.com>
2020-04-06 21:14:04 +0530

    create todo implemented

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,34 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { createTodo } from '../../domain/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+  // Implement creating a new TODO item
+  const newTodoItem = await createTodo(newTodo,jwtToken)
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      newTodoItem
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
