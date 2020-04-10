87b98dbb975486ff86644d8c9bc45a64ff1fdf54
Pravinyo <pravinyo12@gmail.com>
2020-04-10 16:52:10 +0530

    todo search added

--- backend/src/data/esAccess.ts
+++ backend/src/data/esAccess.ts
@@ -3,6 +3,8 @@ import * as httpAwsEs from 'http-aws-es'
 
 import { createLogger } from '../utils/logger'
 import { LogNewTodoEventInES } from '../requests/LogNewTodoEventInES'
+import { Bool } from 'aws-sdk/clients/clouddirectory'
+import { TodoItem } from '../models/TodoItem'
 
 const logger = createLogger('esLogger')
 const esHost = process.env.ES_ENDPOINT
@@ -36,6 +38,38 @@ export class ESAccess {
         type: logDetails.type,
         id: logDetails.id,
         body
+      })
+    }
+
+    /**
+     * Search log entry in elasticSearch for todo items
+     * @param query pattern of logs to be searched
+     *
+     * @param isCompleted boolean value to indicate where to search for
+     * 
+     * @returns array of todoItem
+     */
+    async searchCompletedTodo(query:string,cat : ElasticSearchLogCategory):Promise<any> {
+      logger.info('Search ES for : ', query)
+
+      let logIndex:string
+  
+      if(cat == ElasticSearchLogCategory.DONE_TODO){
+        logIndex = ElasticSearchLogCategory.DONE_TODO
+      }else{
+        logIndex = ElasticSearchLogCategory.NEW_TODO
+      }
+
+      return await this.es.search({
+        index: logIndex,
+        type: 'todos',
+        body:{
+          query:{
+            regexp:{
+              "name": query+".*"
+            }
+          }
+        }
       })
     }
   }
