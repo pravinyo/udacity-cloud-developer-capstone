a1dc1ce16689f10683c0edea034a1b6d1724e848
Pravinyo <pravinyo12@gmail.com>
2020-04-10 15:30:48 +0530

    log event for done and new todo

--- backend/src/lambda/dynamodb/elasticSearchSync.ts
+++ backend/src/lambda/dynamodb/elasticSearchSync.ts
@@ -1,46 +1,44 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
-import * as elasticsearch from 'elasticsearch'
-import * as httpAwsEs from 'http-aws-es'
-
-const esHost = process.env.ES_ENDPOINT
-
-const es = new elasticsearch.Client({
-  hosts: [ esHost ],
-  connectionClass: httpAwsEs
-})
+import { TodoItem } from '../../models/TodoItem'
+import { logNewTodoItemInES, logDoneTodoItemInES } from '../../domain/elasticSearch'
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
   for (const record of event.Records) {
     console.log('Processing record', JSON.stringify(record))
-    if (record.eventName !== 'INSERT') {
-      continue
+    if (record.eventName == 'INSERT') {
+      const newItem = record.dynamodb.NewImage
+
+      const todo : TodoItem = {
+        todoId : newItem.todoId.S,
+        userId : newItem.userId.S,
+        createdAt : newItem.createdAt.S,
+        name : newItem.name.S,
+        attachmentUrl : newItem.attachmentUrl.S,
+        done : newItem.done.BOOL,
+        dueDate : newItem.dueDate.S
+      }
+
+      await logNewTodoItemInES(todo)
+
+    }else if(record.eventName == 'MODIFY'){
+      const newItem = record.dynamodb.NewImage
+
+      const todo : TodoItem = {
+        todoId : newItem.todoId.S,
+        userId : newItem.userId.S,
+        createdAt : newItem.createdAt.S,
+        name : newItem.name.S,
+        attachmentUrl : newItem.attachmentUrl.S,
+        done : newItem.done.BOOL,
+        dueDate : newItem.dueDate.S
+      }
+      
+      if(todo.done){
+        await logDoneTodoItemInES(todo)
+      }
     }
-
-    const newItem = record.dynamodb.NewImage
-
-    const todoId = newItem.todoId.S
-    const userid = newItem.userId.S
-    const created = newItem.createdAt.S
-    const name = newItem.name.S
-    const attachment = newItem.attachmentUrl.S
-
-    const body = {
-      todoId: todoId,
-      userid: userid,
-      attachmentUrl: attachment,
-      todoTitle: name,
-      timestamp: created
-    }
-
-    await es.index({
-      index: 'todos-index',
-      type: 'todos',
-      id: userid,
-      body
-    })
-
   }
 }
