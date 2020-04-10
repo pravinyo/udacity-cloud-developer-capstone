b928220241dbb27844f2da6c0535425bf29cab41
Pravinyo <pravinyo12@gmail.com>
2020-04-10 23:28:39 +0530

    search using simple query

--- backend/src/data/esAccess.ts
+++ backend/src/data/esAccess.ts
@@ -3,7 +3,7 @@ import * as httpAwsEs from 'http-aws-es'
 
 import { createLogger } from '../utils/logger'
 import { LogNewTodoEventInES } from '../requests/LogNewTodoEventInES'
-import { ElasticSearchLogCategory } from '../models/ElasticSearchLogCategory'
+//import { ElasticSearchLogCategory } from '../models/ElasticSearchLogCategory'
 
 const logger = createLogger('esLogger')
 const esHost = process.env.ES_ENDPOINT
@@ -48,30 +48,58 @@ export class ESAccess {
      * 
      * @returns array of todoItem
      */
-    async searchCompletedTodo(query:string,cat : ElasticSearchLogCategory):Promise<any> {
+    async searchNewTodo(query:string):Promise<any> {
       logger.info('Search ES for : ', query)
 
-      let logIndex:string
-  
-      if(cat == ElasticSearchLogCategory.DONE_TODO){
-        logIndex = ElasticSearchLogCategory.DONE_TODO
-      }else{
-        logIndex = ElasticSearchLogCategory.NEW_TODO
-      }
-
-      logger.info("Index is:",logIndex)
-      logger.info("Query:",query)
-      if(query.length==0){
-        query="part"
-      }
-
+      logger.info("Query for new todo list:"+query)
+    
       return await this.es.search({
-        index: logIndex,
+        index: 'todos-index',
         type: 'todos',
         body:{
-          query:{
-            match:{
-              "todoTitle": query
+          "query": {
+            "simple_query_string": {
+              "query": query,
+              "fields": ["todoTitle"]
+            }
+          }
+        }
+      })
+    }
+
+    /**
+     * "query": {
+    "multi_match": {
+      "query": "part google-oauth2|112567431979117533864",
+      "type":       "cross_fields",
+      "fields": ["todoTitle","userid"],
+      "operator":   "and"
+    }
+  }
+
+     */
+
+    /**
+     * Search log entry in elasticSearch for todo items
+     * @param query pattern of logs to be searched
+     *
+     * @param isCompleted boolean value to indicate where to search for
+     * 
+     * @returns array of todoItem
+     */
+    async searchDoneTodo(query:string):Promise<any> {
+      logger.info('Search ES for : ', query)
+
+      logger.info("Query for completed todo list:"+query)
+    
+      return await this.es.search({
+        index: 'done-index',
+        type: 'todos',
+        body:{
+          "query": {
+            "simple_query_string": {
+              "query": query,
+              "fields": ["todoTitle"]
             }
           }
         }
