1b72d8f91af3f536a0733a91899c03e556f1b55c
Pravinyo <pravinyo12@gmail.com>
2020-04-11 00:06:07 +0530

    search new and done todos based on on param

--- backend/src/lambda/elasticSearch/searchTodo.ts
+++ backend/src/lambda/elasticSearch/searchTodo.ts
@@ -4,20 +4,33 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-import { searchNewTodoInES } from '../../domain/elasticSearch'
+import { searchNewTodoInES, searchDoneTodoInES } from '../../domain/elasticSearch'
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
   const request = JSON.parse(event.body)
   
-  /*
+  
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
-*/
+
   console.log("Received request",request)
   console.log("Query is :",request.query)
-  const results = await searchNewTodoInES(request.query)
+  let results:any
+
+  if(request.on === "pending"){
+    results = await searchNewTodoInES(request.query,jwtToken)
+  }else if(request.on ="done"){
+    results = await searchDoneTodoInES(request.query,jwtToken)
+  }else{
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        results:"Invalid param value for On parameter"
+      })
+    }
+  }
 
   return {
     statusCode: 200,
