73cb2d70c3c2098a15dcc01a44ccff8ade8c8d8e
Pravinyo <pravinyo12@gmail.com>
2020-04-10 16:52:23 +0530

    todo search wrapper added

--- backend/src/domain/elasticSearch.ts
+++ backend/src/domain/elasticSearch.ts
@@ -3,24 +3,61 @@ import { ESAccess } from "../data/esAccess";
 
 const esAccess = new ESAccess()
 
+/**
+   *  Wrapper function to create log of new TodoItem provided by the user.
+   * @param TodoItem item enqueued todo
+   *
+   * @returns nothing
+   */
 export async function logNewTodoItemInES(
     todo:TodoItem
   ){
       await esAccess.log({
-          index:'todos-index',
+          index: ElasticSearchLogCategory.NEW_TODO,
           type:'todos',
           id:todo.userId,
           todo:todo
       })
   }
 
+/**
+   *  Wrapper function to create log of done/completed TodoItem provided by the user.
+   * @param TodoItem item enqueued todo
+   *
+   * @returns nothing
+   */
 export async function logDoneTodoItemInES(
     todo:TodoItem
   ){
       await esAccess.log({
-          index:'done-index',
+          index:ElasticSearchLogCategory.DONE_TODO,
           type:'todos',
           id:todo.userId,
           todo:todo
       })
+  }
+
+
+  /**
+   *  Wrapper function to search name of todo done/completed in ElasticSearch provided by the user.
+   * @param query item name starting with
+   *
+   * @returns any
+   */
+export async function searchDoneTodoInES(
+    query:string
+  ):Promise<any>{
+      return await esAccess.searchCompletedTodo(query,ElasticSearchLogCategory.DONE_TODO)
+  }
+
+    /**
+   *  Wrapper function to search name of todo newly created in ElasticSearch provided by the user.
+   * @param query item name starting with
+   *
+   * @returns any
+   */
+export async function searchNewTodoInES(
+    query:string
+  ):Promise<any>{
+      return await esAccess.searchCompletedTodo(query,ElasticSearchLogCategory.NEW_TODO)
   }
