0f96b870dfcc395d4d29ef44c75c390ec48e3aa9
Pravinyo <pravinyo12@gmail.com>
2020-04-12 13:52:10 +0530

    signature changed to support pagination

--- backend/src/domain/ITodoAccess.ts
+++ backend/src/domain/ITodoAccess.ts
@@ -2,9 +2,10 @@ import { TodoItem } from "../models/TodoItem";
 import { TodoDeleteResponse } from "../models/TodoDeleteResponse";
 import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
 import { TodoUpdateResponse } from "../models/TodoUpdateResponse";
+import { GetAllTodoResponse } from "../models/GetAllTodoResponse";
 
 export interface ITodoAccess{
-    getAllTodos(userId:string): Promise<TodoItem[]>;
+    getAllTodos(userId:string,limit:number,nextKey:any): Promise<GetAllTodoResponse>;
     createTodo(todo: TodoItem): Promise<TodoItem>;
     getTodoBy(todoId:string) : Promise<TodoItem>;
     deleteTodo(todoId: string, userId: string): Promise<TodoDeleteResponse>;
